203b14cbece3276d4664936b35cd7e52d59318bc
Gitesss <mateusz.pawel.grad@gmail.com>
2016-04-12 09:06:09 +0200

    Bed way

---
+++ SaveMyURL/SaveMyURL/Model/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveMyURL.Model
+{
+    public class Group : Entity
+    {
+        public string Name { get; set; }
+        public ICollection<Link> Links { get; set; }
+    }
+}
---
+++ SaveMyURL/SaveMyURL/Model/Link.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveMyURL.Model
+{
+    public class Link : Entity
+    {
+        public string URL { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; set; }
+        public byte[] Image { get; set; }
+
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
