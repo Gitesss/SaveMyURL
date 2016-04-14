05001073fb5dd1a9bb386b226e86021c50e12ac9
Gitesss <mateusz.pawel.grad@gmail.com>
2016-04-14 12:07:50 +0200

    welcome page

--- SaveMyURL/SaveMyURL/Pages/WelcomePage.xaml.cs
+++ SaveMyURL/SaveMyURL/Pages/WelcomePage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Intense.Presentation;
+using SaveMyURL.Presentation;
 
 namespace SaveMyURL.Pages
 {
@@ -11,6 +14,29 @@ namespace SaveMyURL.Pages
         public WelcomePage()
         {
             this.InitializeComponent();
+        }
+
+        private void lstViewEmployees_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = new ShellViewModel();
+            vm.TopItems.Add(new NavigationItem { Icon = "", DisplayName = "Welcomevcxvxcv", PageType = typeof(WelcomePage) });
+            vm.TopItems.Add(new NavigationItem { Icon = "", DisplayName = "Page 1vcxvcxv", PageType = typeof(Page1) });
+            vm.TopItems.Add(new NavigationItem { Icon = "", DisplayName = "Page 2cvcxv", PageType = typeof(Page2) });
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
