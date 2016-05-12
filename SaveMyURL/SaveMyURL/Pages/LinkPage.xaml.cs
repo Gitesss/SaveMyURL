b0684bb8cc4ed5a16f0f2803b5263e09fdd977e3
Gitesss <mateusz.pawel.grad@gmail.com>
2016-05-12 20:36:41 +0200

    add new options like drag and drop text in link Page

--- SaveMyURL/SaveMyURL/Pages/LinkPage.xaml.cs
+++ SaveMyURL/SaveMyURL/Pages/LinkPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using SaveMyURL.Model;
 using SaveMyURL.ViewModel;
@@ -39,6 +41,39 @@ namespace SaveMyURL.Pages
             _linkViewModel = new LinkViewModel(selectedGroup);
             DataContext = _linkViewModel;
             txtGroupName.Text = selectedGroup.Name;
+        }
+
+
+        //e.AcceptedOperation = DataPackageOperation.Copy;
+        private async void TargetTextBox_Drop(object sender, DragEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Outside", true);
+            bool hasText = e.DataView.Contains(StandardDataFormats.Text);
+            // if the result of the drop is not too important (and a text copy should have no impact on source)
+            // we don't need to take the deferral and this will complete the operation faster
+            e.AcceptedOperation = hasText ? DataPackageOperation.Copy : DataPackageOperation.None;
+            if (hasText)
+            {
+                var text = await e.DataView.GetTextAsync();
+                TargetTextBox.Text += text;
+            }
+        }
+
+        private void TargetTextBox_DragEnter(object sender, DragEventArgs e)
+        {
+            /// Change the background of the target
+            VisualStateManager.GoToState(this, "Inside", true);
+            bool hasText = e.DataView.Contains(StandardDataFormats.Text);
+            e.AcceptedOperation = hasText ? DataPackageOperation.Copy : DataPackageOperation.None;
+            if (hasText)
+            {
+                e.DragUIOverride.Caption = "Drop here to insert text";
+            }
+        }
+
+        private void TargetTextBox_DragLeave(object sender, DragEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Outside", true);
         }
     }
 }
